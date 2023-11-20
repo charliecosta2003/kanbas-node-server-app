7978158d5bc42cb0b4535de679a84058dc58efe8
NakedMoleRat8 <charliecosta2003@gmail.com>
2023-11-20 09:46:53 -0500

    Minor fixes

--- lab5.js
+++ lab5.js
@@ -14,20 +14,20 @@ const todos = [
 ];
 
 const Lab5 = (app) => {
-    app.get("/a5/welcome", (req, res) => {
+    app.get("/api/a5/welcome", (req, res) => {
         res.send("Welcome to Assignment 5");
     });
-    app.get("/a5/add/:a/:b", (req, res) => {
+    app.get("/api/a5/add/:a/:b", (req, res) => {
         const {a, b} = req.params;
         const sum = parseInt(a) + parseInt(b);
         res.send(sum.toString());
     });
-    app.get("/a5/subtract/:a/:b", (req, res) => {
+    app.get("/api/a5/subtract/:a/:b", (req, res) => {
         const {a, b} = req.params;
         const sum = parseInt(a) - parseInt(b);
         res.send(sum.toString());
     });
-    app.get("/a5/calculator", (req, res) => {
+    app.get("/api/a5/calculator", (req, res) => {
         const {a, b, operation} = req.query;
         let result = 0;
         switch (operation) {
@@ -42,28 +42,28 @@ const Lab5 = (app) => {
         }
         res.send(result.toString());
     });
-    app.get("/a5/assignment", (req, res) => {
+    app.get("/api/a5/assignment", (req, res) => {
         res.json(assignment);
     });
-    app.get("/a5/assignment/title", (req, res) => {
+    app.get("/api/a5/assignment/title", (req, res) => {
         res.json(assignment.title);
     });
-    app.get("/a5/assignment/title/:newTitle", (req, res) => {
+    app.get("/api/a5/assignment/title/:newTitle", (req, res) => {
         const {newTitle} = req.params;
         assignment.title = newTitle;
         res.json(assignment);
     });
-    app.get("/a5/assignment/score/:newScore", (req, res) => {
+    app.get("/api/a5/assignment/score/:newScore", (req, res) => {
         const {newScore} = req.params;
         assignment.score = newScore;
         res.json(assignment);
     });
-    app.get("/a5/assignment/completed/:newCompleted", (req, res) => {
+    app.get("/api/a5/assignment/completed/:newCompleted", (req, res) => {
         const {newCompleted} = req.params;
         assignment.completed = newCompleted;
         res.json(assignment);
     });
-    app.get("/a5/todos", (req, res) => {
+    app.get("/api/a5/todos", (req, res) => {
         const {completed} = req.query;
         if (completed !== undefined) {
             const completedTodos = todos.filter((t) => t.completed === (completed === "true"));
@@ -72,7 +72,7 @@ const Lab5 = (app) => {
         }
         res.json(todos);
     });
-    app.post("/a5/todos", (req, res) => {
+    app.post("/api/a5/todos", (req, res) => {
         const newTodo = {
             ...req.body,
             id: new Date().getTime(),
@@ -80,7 +80,7 @@ const Lab5 = (app) => {
         todos.push(newTodo);
         res.json(newTodo);
     });
-    app.get("/a5/todos/create", (req, res) => {
+    app.get("/api/a5/todos/create", (req, res) => {
         const newTodo = {
             id: new Date().getTime(),
             title: "New Task",
@@ -89,12 +89,12 @@ const Lab5 = (app) => {
         todos.push(newTodo);
         res.json(todos);
     });
-    app.get("/a5/todos/:id", (req, res) => {
+    app.get("/api/a5/todos/:id", (req, res) => {
         const {id} = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         res.json(todo);
     });
-    app.delete("/a5/todos/:id", (req, res) => {
+    app.delete("/api/a5/todos/:id", (req, res) => {
         const { id } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         if (!todo) {
@@ -107,31 +107,31 @@ const Lab5 = (app) => {
         todos.splice(todos.indexOf(todo), 1);
         res.sendStatus(200);
     });
-    app.get("/a5/todos/:id/delete", (req, res) => {
+    app.get("/api/a5/todos/:id/delete", (req, res) => {
         const {id} = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         todos.splice(todos.indexOf(todo), 1);
         res.json(todos);
     });
-    app.get("/a5/todos/:id/title/:title", (req, res) => {
+    app.get("/api/a5/todos/:id/title/:title", (req, res) => {
         const {id, title} = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         todo.title = title;
         res.json(todos);
     });
-    app.get("/a5/todos/:id/completed/:completed", (req, res) => {
+    app.get("/api/a5/todos/:id/completed/:completed", (req, res) => {
         const {id, completed} = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         todo.completed = (completed === "true");
         res.json(todos);
     });
-    app.get("/a5/todos/:id/description/:description", (req, res) => {
+    app.get("/api/a5/todos/:id/description/:description", (req, res) => {
         const {id, description} = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         todo.description = description;
         res.json(todos);
     });
-    app.put("/a5/todos/:id", (req, res) => {
+    app.put("/api/a5/todos/:id", (req, res) => {
         const { id } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         if (!todo) {
